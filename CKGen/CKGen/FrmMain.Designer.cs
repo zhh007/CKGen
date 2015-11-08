ed848bf9795b87fe6fc2557f7f099c4eef5f036a
zhh007 <zhh9113@hotmail.com>
2015-11-08 16:37:01 +0800

    update

--- CKGen/CKGen/FrmMain.Designer.cs
+++ CKGen/CKGen/FrmMain.Designer.cs
@@ -185,6 +185,7 @@
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "编程辅助工具v1.3";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             this.toolStrip1.ResumeLayout(false);
