4c9337a4676892dadb7d671a447cc8230c45583b
zhh007 <zhh9113@hotmail.com>
2016-05-11 19:51:48 +0800

    update

--- CKGen/CKGen/FrmMain.Designer.cs
+++ CKGen/CKGen/FrmMain.Designer.cs
@@ -93,19 +93,21 @@
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.Location = new System.Drawing.Point(0, 25);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.tabControl2);
-            this.splitContainer1.Panel1MinSize = 200;
+            this.splitContainer1.Panel1MinSize = 220;
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Panel2MinSize = 500;
             this.splitContainer1.Size = new System.Drawing.Size(1061, 585);
-            this.splitContainer1.SplitterDistance = 203;
+            this.splitContainer1.SplitterDistance = 220;
             this.splitContainer1.SplitterWidth = 2;
             this.splitContainer1.TabIndex = 1;
             // 
@@ -117,7 +119,7 @@
             this.tabControl2.Location = new System.Drawing.Point(0, 0);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(203, 585);
+            this.tabControl2.Size = new System.Drawing.Size(220, 585);
             this.tabControl2.TabIndex = 1;
             // 
             // tabPage1
@@ -125,7 +127,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(195, 559);
+            this.tabPage1.Size = new System.Drawing.Size(212, 559);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "数据";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -136,7 +138,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 462);
+            this.tabPage2.Size = new System.Drawing.Size(195, 559);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "模板";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -146,7 +148,7 @@
             this.tbTemp.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tbTemp.Location = new System.Drawing.Point(3, 3);
             this.tbTemp.Name = "tbTemp";
-            this.tbTemp.Size = new System.Drawing.Size(186, 456);
+            this.tbTemp.Size = new System.Drawing.Size(189, 553);
             this.tbTemp.TabIndex = 0;
             this.tbTemp.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tbTemp_NodeMouseDoubleClick);
             // 
@@ -157,7 +159,7 @@
             this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(856, 585);
+            this.tabControl1.Size = new System.Drawing.Size(839, 585);
             this.tabControl1.TabIndex = 0;
             // 
             // imageList1
