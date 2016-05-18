a5cb8efdbccbe5986e5c344b7d3a1a302c5f6dc5
zhh007 <zhh9113@hotmail.com>
2016-05-18 11:12:17 +0800

    add stauts bar

--- CKGen/CKGen/FrmMain.Designer.cs
+++ CKGen/CKGen/FrmMain.Designer.cs
@@ -38,6 +38,7 @@
             this.tsBtnQuery = new System.Windows.Forms.ToolStripButton();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.dockPanel = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -121,8 +122,16 @@
             this.dockPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dockPanel.Location = new System.Drawing.Point(0, 25);
             this.dockPanel.Name = "dockPanel";
-            this.dockPanel.Size = new System.Drawing.Size(1061, 585);
+            this.dockPanel.Size = new System.Drawing.Size(1061, 563);
             this.dockPanel.TabIndex = 1;
+            // 
+            // statusBar
+            // 
+            this.statusBar.Location = new System.Drawing.Point(0, 588);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(1061, 22);
+            this.statusBar.TabIndex = 3;
+            this.statusBar.Text = "statusStrip1";
             // 
             // FrmMain
             // 
@@ -130,6 +139,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1061, 610);
             this.Controls.Add(this.dockPanel);
+            this.Controls.Add(this.statusBar);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmMain";
@@ -153,5 +163,6 @@
         private System.Windows.Forms.ToolStripButton tsbtnSaveSchema;
         private System.Windows.Forms.ToolStripButton tsBtnReloadSchema;
         private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel;
+        private System.Windows.Forms.StatusStrip statusBar;
     }
 }
